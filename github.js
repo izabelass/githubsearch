c9ddf9e3788980cf31ed056dea21438c03b0a789
Izabela Soares <soares.izabela96@gmail.com>
2022-09-13 10:02:59 -0300

    fetch and display repos

--- github.js
+++ github.js
@@ -1,7 +1,9 @@
 class GitHub {
     constructor() {
-        this.client_id = ''; //config githuh oauth
-        this.client_secret = ''; //config githuh oauth
+        this.client_id = '9cf2857af487c83e7001'; //config githuh oauth
+        this.client_secret = '91378ac7eee84de103b64c38030b80e8e5bbd0c5'; //config githuh oauth
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
@@ -9,11 +11,19 @@ class GitHub {
         `https://api.github.com/users/${user}
         ?client_id=${this.client_id}
         &client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(
+            `https://api.github.com/users/${user}/repos
+            ?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}
+            &client_secret=${this.client_secret}`);
         
         const profileData = await profileResponse.json();
 
+        const repos = await repoResponse.json();
+
         return {
-            profile // equals to profile:profileData
+            profile, // equals to profile:profileData
+            repos
         }
 
     }
